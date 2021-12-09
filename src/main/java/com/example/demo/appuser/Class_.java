e66b9620cadabd3516c1623ff1a9199d0478f78b
Theodorulus <theo_tudorache@yahoo.com>
2021-12-09 21:37:38 +0200

    fixed wrong files location

--- src/main/java/com/example/demo/appuser/Class_.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.appuser;
-
-import java.util.List;
-
-public class Class_ {
-    private String name;
-    private List<Student> students;
-    private List<Teacher> teachers;
-}
